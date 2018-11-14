367e0ae2b9f35a09a2d091789d6abe9e5dca61e4
AlesiaSk <alesya213@gmail.com>
2018-11-14 23:29:55 +0300

    Realization without MOVE and COPY

--- FileSystem/Source.cpp
+++ FileSystem/Source.cpp
@@ -85,10 +85,27 @@ void add_catalog(element **begin, element *beg, element **end, catalog cur_catal
 }
 
 catalog DEL(catalog dir, file file) {
+	int curr_size = dir.files.size();
 	for (int i = 0; i < dir.files.size(); i++) {
 		if (dir.files[i].name == file.name) {
 			dir.files.erase(dir.files.begin() + i);
 		}
+	}
+	if (curr_size == dir.files.size()) {
+		cout << "Ñan't find this file";
+	}
+	return dir;
+}
+
+catalog RMDIR(catalog dir, string deletable) {
+	int curr_size = dir.catalogs.size();
+	for (int i = 0; i < dir.catalogs.size(); i++) {
+		if (dir.catalogs[i].name == deletable) {
+			dir.catalogs.erase(dir.catalogs.begin() + i);
+		}
+	}
+	if (curr_size == dir.catalogs.size()) {
+		cout << "Ñan't find this file";
 	}
 	return dir;
 }
@@ -132,34 +149,80 @@ void main() {
 	string filename;
 	string type;
 	string req;
+	catalog dir;
 	catalog root = MD("root");
-	vector<catalog> catalogs;
-	catalog curr_catalog = root;
-	catalog prev_catalog = root;
-	//cout << "Choose 1 - Create a file; 2 - Create directory; 3 - Show files from directory; 4 - Delete file; 5 - Move file to another directory; 6 - Delete directory" << endl;
+	vector<catalog*> path;
+	catalog* curr_catalog = &root;
+
+	path.push_back(&root);
 	while (true)
 	{
-		cout << curr_catalog.name << ">";
+		for (int i = 0; i < path.size(); i++) {
+			if (i == 0) {
+				cout << (*path[i]).name;
+			}
+			else {
+				cout << "\\" << (*path[i]).name;
+			}
+		}
+		cout << ">";
 		cin >> cases;
 		req = cases.substr(0, cases.find(" "));
+	
 		if (req == "cf") {
 			cin >> cases;
 			type = cases.substr(cases.length() - 3, 3);
 			filename = cases.substr(cases.find(" ") + 1, cases.length() - 4);
-			curr_catalog.files.push_back(CREATE_FILE(filename, type));
+			(*curr_catalog).files.push_back(CREATE_FILE(filename, type));
 		}
 		else if (req == "md"){
 			cin >> filename;
-			curr_catalog.catalogs.push_back(MD(filename));
+			(*curr_catalog).catalogs.push_back(MD(filename));
 		}
 		else if (req == "dir") {
-			DIR(curr_catalog);
-			for (int i = 0; i < curr_catalog.catalogs.size(); i++) {
-				cout << curr_catalog.catalogs[i].name << endl;
+			DIR(*curr_catalog);
+			for (int i = 0; i < (*curr_catalog).catalogs.size(); i++) {
+				cout << (*curr_catalog).catalogs[i].name << endl;
 			}
 		}
 		else if (req == "del") {
-			curr_catalog = DEL(curr_catalog, curr_catalog.files[0]);
+			(*curr_catalog) = DEL(*curr_catalog, (*curr_catalog).files[0]);
+		}
+		else if (req == "cd") {
+			cin >> filename;
+			for (int i = 0; i < (*curr_catalog).catalogs.size(); i++) {
+				if ((*curr_catalog).catalogs[i].name == filename) {
+					dir = (*curr_catalog).catalogs[i];
+					curr_catalog = &dir;
+					path.push_back(curr_catalog);
+				}
+				else {
+					cout << "Directory not found" << endl;
+				}
+			}
+		}
+		else if (req == "back"){
+			if ((*curr_catalog).name != root.name) {
+				curr_catalog = path[path.size() - 2];
+				path.erase(path.begin() + (path.size() - 1));
+			}
+		}
+		else if (req == "rmdir") {
+			cin >> filename;
+			*curr_catalog = RMDIR(*curr_catalog, filename);
+		}
+		else if (req == "move") {
+			cin >> filename;
+			*curr_catalog = RMDIR(*curr_catalog, filename);
+		}
+		else if (req == "help") {
+			cout << "md     create directory"<< endl;
+			cout << "cf     create file" << endl;
+			cout << "cd     change directory" << endl;
+			cout << "back   come back to parent directory" << endl;
+			cout << "del    delete file" << endl;
+			cout << "rmdir  delete directory" << endl;
+			cout << endl;
 		}
 	}
 	
